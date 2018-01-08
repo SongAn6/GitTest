c4e1dac8af4b72419db35e0f954e80c7408bb721
SongAn8 <849953550@qq.com>
2018-01-08 11:57:02 +0800

    测试

--- src/Test.java
+++ src/Test.java
@@ -2,5 +2,8 @@ public class Test {
     public static void main(String []args){
 
           System.out.print("你好啊！");
+          System.out.print("你好啊！");
+          System.out.print("快捷键操作");
+          System.out.print("你好啊！");
     }
 }
